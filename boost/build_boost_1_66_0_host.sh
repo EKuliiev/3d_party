0f0681e1f7271734efdb06ae548e4ce6d2e02dd7
Elvis Kuliiev <ekuliiev@luxoft.com>
2018-10-01 16:33:28 +0300

    Fix build scripts

--- boost/build_boost_1_66_0_host.sh
+++ boost/build_boost_1_66_0_host.sh
@@ -2,10 +2,17 @@
 
 readonly script_location="$( dirname $( realpath -s $0 ) )"
 
+set -e
+
 cd ${script_location}/boost_1_66_0
 ./bootstrap.sh --prefix=${script_location}/install/usr
 b2_args+=" link=static cxxflags=\"-fPIC\" "
 b2_args+=" --with-atomic --with-thread --with-date_time --with-chrono --with-system --with-filesystem --with-serialization --with-program_options "
-./b2 $b2_args headers
-./b2 $b2_args install
+command_to_perform="./b2 $b2_args headers"
+echo " ---> Running '$command_to_perform'" 
+eval $command_to_perform
+command_to_perform="./b2 $b2_args install"
+echo " ---> Running '$command_to_perform'" 
+eval $command_to_perform
+
 cd ..
--- boost/build_boost_1_66_0_qnx.sh
+++ boost/build_boost_1_66_0_qnx.sh
@@ -2,12 +2,19 @@
 
 source /opt/qnx700/qnxsdp-env.sh
 
+set -e
+
 readonly script_location="$( dirname $( realpath -s $0 ) )"
 
 cd ${script_location}/boost_1_66_0
 ./bootstrap.sh --prefix=${script_location}/install/usr
-b2_args+=" link=static toolset=qcc address-model=32  cxxflags='-fPIC -Vgcc_ntoaarch64le' target-os=qnxnto "
+b2_args+=" link=static toolset=qcc address-model=64 target-os=qnxnto cxxflags='-fPIC -Vgcc_ntoaarch64le' "
 b2_args+=" --with-atomic --with-thread --with-date_time --with-chrono --with-system --with-filesystem --with-serialization --with-program_options "
-./b2 $b2_args headers
-./b2 $b2_args install
+command_to_perform="./b2 $b2_args headers"
+echo " ---> Running '$command_to_perform'" 
+eval $command_to_perform
+command_to_perform="./b2 $b2_args install"
+echo " ---> Running '$command_to_perform'" 
+eval $command_to_perform
+
 cd ..
